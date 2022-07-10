762dfb77e725b15872e1f2b691b62a3b21033a3e
Jimmy Li <jimmyli2002@gmail.com>
2022-07-10 10:27:36 -0700

    add codegen api and auth utils

--- cmd/control-plane/targets.go
+++ cmd/control-plane/targets.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
-func (a *app) targets(c *fiber.Ctx) error {
+/* Returns available connection targets, this is NOT exhaustive
+ */
+func (a *app) getTargets(c *fiber.Ctx) error {
+	fmt.Println("out", c.Locals("user"))
+	st, er := a.SignToken("jimmytoken")
+	fmt.Println(st, er)
 	return c.SendString("TODO")
 }
