c63efb6856160524ea580e8d269d397d6dd26122
jimmyl02 <jimmyli2002@gmail.com>
2023-01-03 21:51:39 +0000

    fix: reply to jrpc to avoid timeout

--- cmd/client/client_windows.go
+++ cmd/client/client_windows.go
@@ -7,5 +7,5 @@ import "fmt"
 // ClientServeProfile instructs the rVPN daemon to serve as a VPN server for a target via rpc
 func ClientServeProfile(profile string) {
 	// NOTE: it is currently not supported for the windows client to serve as a VPN server
-	fmt.Println("ERROR: windows rVPN client is not supported to server as a VPN server")
+	fmt.Println("ERROR: windows rVPN client is not supported to serve as a VPN server")
 }
