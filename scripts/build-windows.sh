5bd5fb3b595a56adb9b10fd9b247bf671f0875a2
Ethan Wu <ethanwu10@gmail.com>
2023-01-15 17:53:47 -0800

    chore: clean up wails files
    Removed many Wails-generated files from git that are not stable between
    host platforms used to generate them:
    - package.json.md5 - this is used for caching and thus breaks caching
      logic when it is present from the git checkout; also, it is not
      generated using normalized line endings, causing git working directory
      line-ending settings to thrash the checksum file
    - wailsjs/ generated code - Wails sets mode 0755 recursively for some
      reason, which causes differences between unix and windows when git
      tracks file modes on the files
    
    Clean up windows build script.

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-# switch directory
-cd "$(dirname "$(dirname "$0")")"
-cd "../"
-
-# initial wails build
-cd "./cmd/client_gui"
-wails build
+# switch directory to repo root
+cd "$(dirname "$0")/.."
 
 # build cli client
-cd "../../"
 go build -o "./cmd/client_gui/build/bin/rvpnc.exe" "./cmd/client"
 
 # copy vendored wintun to wails build dir
 cp "./vendored/wintun.dll" "./cmd/client_gui/build/bin/"
 
 # wails build nsis installer
-cd "./cmd/client_gui"
-wails build -nsis
+(cd "./cmd/client_gui" && wails build -platform "windows/amd64" -nsis)
