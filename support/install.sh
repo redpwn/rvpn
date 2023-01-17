caa96a5599b191d757f37c6393a67427557e4dcd
jimmyl02 <jimmyli2002@gmail.com>
2023-01-17 12:11:24 -0800

    feat: update install script

--- support/install.sh
+++ support/install.sh
@@ -5,6 +5,12 @@ set -euo pipefail
 version="v0.0.1"
 arch="amd64" # FIXME
 
+enable_ip4_forwarding() {
+    # arg $1 is the sudo string
+    echo "net.ipv4.ip_forward = 1" >> "$1" tee -a /etc/sysctl.conf
+    $1 sysctl -p /etc/sysctl.conf
+}
+
 err() {
     echo "$@" >&2
 }
@@ -32,14 +38,29 @@ main() {
     fi
 
     # download tar
-    curl -o rvpn.tar.gz "https://github.com/redpwn/rvpn/releases/download/$version/rvpn_linux_$arch.tar.gz"
+    curl -sSLo rvpn.tar.gz "https://github.com/redpwn/rvpn/releases/download/$version/rvpn_linux_$arch.tar.gz"
 
     # extract tar
     tar -xzvf rvpn.tar.gz
 
+    # ask user if they want to enable ip forwarding if not already enabled
+    if [ "$(cat /proc/sys/net/ipv4/ip_forward)" -eq 0 ]; then
+        echo "Do you wish to enable IP Forwarding (only used if this device is a VPN server)? [1,2]"
+        select yn in "Yes" "No"; do
+            case $yn in
+                Yes ) enable_ip4_forwarding $sudo ; break;;
+                No ) exit;;
+            esac
+        done
+    fi
+
     # install rvpn and rvpn service
-    $sudo install rvpn_linux_$arch/systemd/rvpn.service /etc/systemd/system/
-    $sudo install rvpn_linux_$arch/bin/rvpn /usr/local/bin
+    $sudo install -Dm 644 -t /usr/local/lib/systemd/system/ rvpn_linux_$arch/systemd/systemd/rvpn.service
+    $sudo install -m 755 -t /usr/local/bin/ rvpn_linux_$arch/bin/rvpn
+
+    # print success and remind user to allow rvpn serve port (21820) through firewall if serving
+    echo "Successfully installed rVPN!"
+    echo "If using this device as a VPN server please allow rVPN serve port (default 21820) through firewall"
 }
 
 main
